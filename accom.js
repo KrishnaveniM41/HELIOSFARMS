0e67d8d9d2917bb5e0c4dd6e61608dc52e676f39
Krishnaveni M <85820352+KrishnaveniM41@users.noreply.github.com>
2024-02-26 15:05:44 +0530

    Add files via upload

---
+++ accom.js
@@ -0,0 +1,76 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+// Remove the event listener for closing on navLinks click
+/*
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+*/
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container .section__subheader", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// room container
+ScrollReveal().reveal(".room__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// feature container
+ScrollReveal().reveal(".feature__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// news container
+ScrollReveal().reveal(".news__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// Add this script for dropdown behavior
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  dropdowns.forEach(function (dropdown) {
+    dropdown.addEventListener("click", function () {
+      this.querySelector(".dropdown-content").classList.toggle("show");
+    });
+  });
+
+  window.addEventListener("click", function (event) {
+    dropdowns.forEach(function (dropdown) {
+      if (!dropdown.contains(event.target)) {
+        dropdown.querySelector(".dropdown-content").classList.remove("show");
+      }
+    });
+  });
+});
